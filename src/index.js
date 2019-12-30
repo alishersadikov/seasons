e1f5ca5eb316af91daa5ae78b0772d29bda73d2d
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-29 20:31:49 -0600

    Set state with async call

--- src/index.js
+++ src/index.js
@@ -5,17 +5,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    // the only time direct assignment is valid
     this.state = { lat: null };
-  }
 
-  // React says we hav eto define render
-  render() {
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        // we called setState, no direct assignment!
+        this.setState({ lat: position.coords.latitude });
+      },
       error => console.log(error)
     );
+  }
 
-    return <div>Latitude: </div>;
+  // React says we have to define render
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
