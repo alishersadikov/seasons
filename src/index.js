31493ea5445eb1f13b05d446d674f88f1842f863
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-29 21:20:58 -0600

    Alternate state init

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    // the only time direct assignment is valid
-    this.state = { lat: null, errorMessage: "" };
-  }
+  // Babel does the constructor init for you
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
